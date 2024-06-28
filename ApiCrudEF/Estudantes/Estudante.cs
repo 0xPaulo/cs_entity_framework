fdb505e237cfaafbe16ddd213564737e3f7246bb
Paulo <102301140+0xPaulo@users.noreply.github.com>
2024-06-28 13:39:17 -0300

    novo estudante class

---
+++ ApiCrudEF/Estudantes/Estudante.cs
@@ -0,0 +1,16 @@
+namespace ApiCrudEF.Estudantes
+{
+    public class Estudante
+    {
+        public Guid Id { get; init; }
+        public string Nome { get; private set; }
+        public bool Ativo { get; private set; }
+
+        public Estudante(string nome)
+        {
+            this.Nome = nome;
+            Id = Guid.NewGuid();
+            Ativo = true;
+        }
+    }
+}
